4bb0f037de0d01a9232e7b3a9e9acbf3bc59329a
Spinu2b <spinu2b@gmail.com>
2021-04-24 18:40:47 +0200

    Development

--- acbmc/scene_setups/bone_world_matrices/transforms/disp_chain_bones_trans_fact.py
+++ acbmc/scene_setups/bone_world_matrices/transforms/disp_chain_bones_trans_fact.py
@@ -1,8 +1,27 @@
+import random
+from acbmc.model.animated_character.model.math.vector3d import Vector3d
 from typing import List
 from acbmc.util.model.transform_node import TransformNode
+from mathutils import Quaternion
 
 
 class DisplacedChainedBonesTransformsFactory:
   @classmethod
   def get_displaced_transforms_for(cls, bones_count: int, seed: int) -> List[TransformNode]:
-    raise NotImplementedError
+    random.seed(seed)
+    displaced_coordinates = [random.uniform(-1.0, 1.0) for _ in range(bones_count)]
+    displaced_scale_coordinates = [random.uniform(0.5, 1.2) for _ in range(bones_count)]
+
+    result = []  # type: List[TransformNode]
+    home_rotation = Quaternion()
+
+    for index in range(bones_count):
+      result.append(
+        TransformNode.construct_with(
+          position=Vector3d(displaced_coordinates[index], displaced_coordinates[index], displaced_coordinates[index]),
+          rotation=home_rotation,
+          scale=Vector3d(displaced_scale_coordinates[index], displaced_scale_coordinates[index], displaced_scale_coordinates[index])
+        )
+      )
+
+    return result
--- acbmc/scene_setups/util/armature/blender_armature_animator.py
+++ acbmc/scene_setups/util/armature/blender_armature_animator.py
@@ -1,16 +1,26 @@
-from typing import List
+from typing import Dict, List
 from bpy.types import Object
 from acbmc.util.model.tree_hierarchy import TreeHierarchy
 from acbmc.util.model.transform_node import TransformNode
 
 
 class BlenderArmatureAnimator:
-    @classmethod
-    def for_armature(blender_armature_obj: Object) -> 'BlenderArmatureAnimator':
-        raise NotImplementedError
+    def __init__(self, blender_armature_obj: Object):
+        self.blender_armature_obj = blender_armature_obj
+        self.bones_keyframes = dict()  # type: Dict[int, Dict[str, TransformNode]]
 
-    def animate_bone(name: str, local_transform: TransformNode, keyframe_number: int) -> 'BlenderArmatureAnimator':
-        raise NotImplementedError
+    def _add_keyframe_if_does_not_exist(self, keyframe_number: int):
+        if keyframe_number not in self.bones_keyframes:
+            self.bones_keyframes[keyframe_number] = dict()
+
+    @classmethod
+    def for_armature(cls, blender_armature_obj: Object) -> 'BlenderArmatureAnimator':
+        return BlenderArmatureAnimator(blender_armature_obj)
+
+    def animate_bone(self, name: str, local_transform: TransformNode, keyframe_number: int) -> 'BlenderArmatureAnimator':
+        self._add_keyframe_if_does_not_exist(keyframe_number)
+        self.bones_keyframes[keyframe_number][name] = local_transform.copy()
+        return self
 
     def commit(self) -> List[TreeHierarchy]:
         raise NotImplementedError
