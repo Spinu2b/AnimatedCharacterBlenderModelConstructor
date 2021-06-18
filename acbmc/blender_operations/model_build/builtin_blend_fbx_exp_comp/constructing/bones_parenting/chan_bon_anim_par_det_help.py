d82903f131d3e9bd236218dea501d2e7265a247c
Spinu2b <spinu2b@gmail.com>
2021-06-19 00:37:38 +0200

    Determining parents amongst channel bones

--- acbmc/blender_operations/model_build/builtin_blend_fbx_exp_comp/constructing/bones_parenting/chan_bon_anim_par_det_help.py
+++ acbmc/blender_operations/model_build/builtin_blend_fbx_exp_comp/constructing/bones_parenting/chan_bon_anim_par_det_help.py
@@ -1,6 +1,8 @@
-
 from typing import Dict, List
 from bpy.types import PoseBone
+from acbmc.blender_operations.model_build.builtin_blend_fbx_exp_comp \
+    .armature.uni_arm_with_deform_sets_bones_nam_help \
+         import UnifiedArmatureWithDeformSetsBonesNamingHelper
 from acbmc.model.animated_character.model \
     .channel_hierarchies_desc.channel_hierarchy import ChannelHierarchy
 
@@ -13,4 +15,23 @@ class ChannelBoneAnimatedParentingDeterminerHelper:
         armature_pose_bones: List[PoseBone],
         channel_hierarchies: Dict[str, ChannelHierarchy]
     ) -> List[PoseBone]:
-        raise NotImplementedError
+        result = []  # type: List[PoseBone]
+
+        pose_bone_channel_id = \
+                UnifiedArmatureWithDeformSetsBonesNamingHelper \
+                    .get_channel_id_from_channel_bone_name(pose_bone.name)
+
+        for chan_hierarchy in channel_hierarchies.values():
+            if pose_bone_channel_id in chan_hierarchy.channel_hierarchy.parenting:
+                parent_channel_id = chan_hierarchy.channel_hierarchy.parenting[pose_bone_channel_id]
+
+                if parent_channel_id not in list(map(
+                    lambda x: UnifiedArmatureWithDeformSetsBonesNamingHelper \
+                        .get_channel_id_from_channel_bone_name(x.name), result)):
+                    result \
+                        .append(
+                            next(y for y in armature_pose_bones
+                             if UnifiedArmatureWithDeformSetsBonesNamingHelper \
+                                .get_channel_id_from_channel_bone_name(y.name) == parent_channel_id))
+                    
+        return result
